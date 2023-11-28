8a86222e18e836002135b848bc6696b3a87e7c30
Ivan L. Murillo <diskman199@gmail.com>
2023-11-28 11:26:24 -0600

    feat: Added first version of algorithm and tested with chart

--- mainwindow.py
+++ mainwindow.py
@@ -5,10 +5,17 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 import numpy as np
-import gc
 
 LEFT_CLICK = 1
 RIGHT_CLICK = 3
+DELAY = 100
+CONTOUR_DOTS = 100
+
+CUSTOM_COLORS = [ '#fc72e8', '#fc72e8', '#fd88eb', '#fe9eeb',
+'#ffadec', '#ffc3ec', '#ffdcec', '#ffe9ec', '#fff2ec',
+'#fff8ec',  '#ffffff', '#ccd3ff',
+'#b2baff', '#99c1ff', '#7fb8ff', '#66afff', '#4ca6ff', 
+'#339dff', '#1994ff', '#007bfa', '#007bfa' ]
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -128,7 +135,6 @@ class MainWindow(QMainWindow):
         if self.mu <= 0 or self.mu > 10:
             return self.print_error("Datos erroneos", "Mu debe estar entre 0.01 y 0.1")
         
-        self.init_table()
         self.ui.n_neurons.setEnabled(False)
         self.is_running = True
         self.run_algorithm()
@@ -137,6 +143,139 @@ class MainWindow(QMainWindow):
         self.plot_solutions = []
         error = 1e10
         epochs = 0
+        input_size = 2
+        output_size = 1
+
+        w_input_hidden = np.random.rand(input_size, self.n)
+        b_hidden = np.random.rand(1, self.n)
+        w_hidden_output = np.random.rand(self.n, output_size)
+        b_output = np.random.rand(1, output_size)
+
+        X = np.array([pattern[:input_size] for pattern in self.data])
+        Y = np.array([pattern[-output_size:] for pattern in self.data])
+
+        n_samples = len(self.data)
+        n_weights = self.n*(input_size + 1) + output_size*(self.n + 1)
+
+        while error > self.target_error and epochs < self.max_iterations:
+            hidden_layer_input = np.dot(X, w_input_hidden) + b_hidden
+            hidden_layer_output = self.activation_function(hidden_layer_input)
+            output_layer_input = np.dot(hidden_layer_output, w_hidden_output) + b_output
+            output_layer_output = self.activation_function(output_layer_input)
+
+            # Cálculo del error cuadrático medio de la neurona de salida
+            error = np.mean((Y - output_layer_output)**2)
+
+            # Tamaño = patrones * pesos
+            Jacobian = np.zeros((n_samples, n_weights))
+            for sample in range(n_samples):
+                x = X[sample]
+                z_h = hidden_layer_input[sample]
+                h = hidden_layer_output[sample]
+                z_o = output_layer_input[sample]
+                o = output_layer_output[sample]
+
+                start_idx = 0
+                # 2 entradas * n neuronas de capa oculta
+                for i in range(input_size):
+                    for j in range(self.n):
+                        Jacobian[sample, start_idx] = x[i] + self.activation_function_derivative(z_h[j])*w_hidden_output[j]
+                        start_idx += 1
+                
+                # 1 bias por neurona
+                for i in range(self.n):
+                    Jacobian[sample, start_idx] = self.activation_function_derivative(z_h[i])*w_hidden_output[i]
+                    start_idx += 1
+
+                # n entradas (salida capa oculta) * i de capa de salida (1) 
+                for i in range(self.n):
+                    for j in range(output_size):
+                        Jacobian[sample, start_idx] = h[i] * self.activation_function_derivative(z_o[j])
+                        start_idx += 1
+
+                # 1 bias por neurona de salida
+                for i in range(output_size):
+                    Jacobian[sample, start_idx] = self.activation_function_derivative(z_o[i])
+                    start_idx += 1
+
+            E = Y - output_layer_output
+
+            # (JkT*Jk + uI)^-1 * JkT*ek
+            delta = np.linalg.pinv(Jacobian.T @ Jacobian + self.mu * np.eye(Jacobian.shape[1])) @ Jacobian.T @ E
+
+            w_input_hidden = w_input_hidden + self.learning_rate*delta[:input_size*self.n].reshape((input_size, self.n))
+            b_hidden = b_hidden + self.learning_rate*delta[input_size*self.n:input_size*self.n + self.n].reshape((1, self.n))
+            w_hidden_output = w_hidden_output + self.learning_rate*delta[input_size * self.n + self.n:-output_size].reshape((self.n, output_size))
+            b_output = b_output + self.learning_rate*delta[-output_size:].reshape((1, output_size))
+
+            epochs += 1
+            if epochs % 10 == 0:
+                self.plot_solutions.append({
+                    "error": error,
+                    "epoch": epochs,
+                    "solution": {
+                        "wh": w_input_hidden,
+                        "bh": b_hidden,
+                        "wo": w_hidden_output,
+                        "bo": b_output
+                    }
+                })
+
+        if self.ui.just_output.isChecked():
+            self.plot_solutions = [ self.plot_solutions[-1] ]
+        print(f"Entrenamiento finalizado en {epochs} épocas con error {error}")
+        self.plot_with_delay()
+
+    def activation_function(self, v):
+        return 1/(1 + np.exp(-v))
+    
+    def activation_function_derivative(self, v):
+        return self.activation_function(v) * (1 - self.activation_function(v))
+    
+    def classificate(self, solution, X, Y):
+        hidden_layer_weights = solution["wh"]
+        hidden_layer_bias = solution["bh"]
+        output_layer_weights = solution["wo"]
+        output_layer_bias = solution["bo"]
+
+        input_data = np.column_stack((X.ravel(), Y.ravel())).T
+
+        hidden_layer_input = np.dot(hidden_layer_weights.T, input_data) + hidden_layer_bias.T
+        hidden_layer_output = self.activation_function(hidden_layer_input)
+        output_layer_input = np.dot(output_layer_weights.T, hidden_layer_output) + output_layer_bias
+        output_layer_output = self.activation_function(output_layer_input)
+        return output_layer_output.reshape(X.shape)
+
+    def plot_with_delay(self):
+        if len(self.plot_solutions) == 0:
+            self.is_running = False
+            self.ui.n_neurons.setEnabled(True)
+            self.plot_solutions.clear()
+            return
+        
+        plot = self.plot_solutions.pop(0)
+        solution = plot['solution']
+        error = plot['error']
+        epoch = plot['epoch']
+
+        self.ui.iteration_label.setText(str(epoch))
+        self.ui.error_label.setText(f"{error:.4g}")
+        
+        swep = np.linspace(-10, 10, CONTOUR_DOTS)
+        X, Y = np.meshgrid(swep, swep)
+        Z = self.classificate(solution, X, Y)
+
+        custom_colors = CUSTOM_COLORS
+        custom_levels = np.linspace(-1, 1, len(custom_colors))
+        
+        colorbar = self.ax.contourf(X, Y, Z, levels=custom_levels, colors=custom_colors)
+
+        if self.colorbar is None:
+            self.colorbar = colorbar
+            self.figure.colorbar(colorbar)
+
+        self.canvas.draw()
+        QTimer.singleShot(DELAY, self.plot_with_delay)
 
     def initilize_algorithm(self):
         self.is_running = False
